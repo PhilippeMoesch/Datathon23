7013101a2f005786fce6e43e981c595a1aae81a8
PhilippeMoesch <philipmoesch@gmail.com>
2023-05-07 11:43:10 +0200

    update

--- imbalance-model/feature_engineering.py
+++ imbalance-model/feature_engineering.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
 
+
 def floor_datetime(x: pd.Series, raster: str = "15min") -> pd.Series:
     return x.dt.tz_convert("UTC").dt.floor(raster).dt.tz_convert("Europe/Zurich")
 
 
-def q90(x):
-    return x.quantile(0.9)
+def q80(x):
+    return x.quantile(0.8)
 
 
-def q10(x):
-    return x.quantile(0.1)
+def q20(x):
+    return x.quantile(0.2)
 
 
 def aggregate_indicator(df: pd.DataFrame) -> pd.DataFrame:
@@ -32,13 +33,20 @@ def aggregate_indicator(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def aggregate_frequency(df: pd.DataFrame) -> pd.DataFrame:
-    df["ts_15min"] = floor_datetime(df["ts"])
+    
     df["va"] -= 50
     df["va"] = np.where(df["va"].abs() <= 0.02, 0, df["va"])
+    df = cyclicity_removal(df, time_unit="minute")
+
+    df["ts_15min"] = floor_datetime(df["ts"])
     df = df.loc[(df["ts"] - pd.to_timedelta(5, unit="min")) <= df["ts_15min"]]
     df = (
         df.groupby("ts_15min", as_index=False)
-        .agg(frequency_q90=("va", q90), frequency_q10=("va", q10))
+        .agg(   frequency_q80=("va", q80), 
+                frequency_q20=("va", q20),
+                frequency_mean=("va", np.mean),
+                frequency_std=("va", np.std)
+            )
         .rename(columns={"ts_15min": "ts"})
     )
     return df
@@ -80,6 +88,19 @@ def shift_real_time_data(df: pd.DataFrame) -> pd.DataFrame:
     return df.drop(columns=cols_to_shift)
 
 
+def holiday(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.assign(holiday=0)
+    # data_ = data.set_index('ts')
+    # loop over the list of dates and set weekday to zero for those dates
+    for date in ['2022-10-03','2022-12-25','2022-12-26','2023-01-01']:
+        mask = df['ts'].dt.date == pd.to_datetime(date).date()
+        df.loc[mask, 'holiday'] = 1
+
+    # set the weekends also as holidays
+    df.loc[df['ts'].dt.dayofweek == 5, 'holiday'] = 1
+    df.loc[df['ts'].dt.dayofweek == 6, 'holiday'] = 1
+    return df
+
 def fillna_with_mean(df: pd.DataFrame) -> pd.DataFrame:
     return df.fillna(df.mean())
 
@@ -104,7 +125,7 @@ def cyclicity_removal(df: pd.DataFrame, time_unit = ['hour','minute']) -> pd.Dat
     df_ave_time = df_ave_time.reset_index()
     # divide the data by its mean
     merged_df = pd.merge(df, df_ave_time, on=time_unit)
-    merged_df= merged_df.set_index('ts')# .drop(columns='ts_y')
+    merged_df= merged_df.set_index('ts_x').drop(columns='ts_y')
     cols_x = merged_df.filter(like='_x').columns
     cols_y = merged_df.filter(like='_y').columns
     eps = 1e-1
@@ -120,15 +141,18 @@ def cyclicity_removal(df: pd.DataFrame, time_unit = ['hour','minute']) -> pd.Dat
     return output_df
 
 def feature_pipeline(train_or_test: str = "train") -> pd.DataFrame:
-    indicator = pd.read_parquet(f"data/{train_or_test}/indicator.parquet")
+    indicator = pd.read_parquet(f"../{train_or_test}/indicator.parquet")
     indicator = aggregate_indicator(indicator)
-    main = pd.read_parquet(f"data/{train_or_test}/main.parquet")
+    main = pd.read_parquet(f"../{train_or_test}/main.parquet")
     main = aggregate_wind(main)
     main = shift_real_time_data(main)
-   
+    
     main = cyclicity_removal(main)
     main = cyclicity_removal(main, time_unit="dayofweek")
-    frequency = pd.read_parquet(f"data/{train_or_test}/frequency.parquet")
+    
+    main = holiday(main)
+
+    frequency = pd.read_parquet(f"../{train_or_test}/frequency.parquet")
     frequency = aggregate_frequency(frequency)
     main = main.merge(frequency, how="left").merge(indicator, how="left")
     main = fillna_with_mean(main)
